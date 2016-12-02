6c4a33dac101a8bec1e3f52df6208f82380ac4d2
VijayIndia <vxm47990@ucmo.edu>
2016-12-02 01:58:17 -0500

    Update Time_Conversion.java

--- HackerRank/Algorithms/Warmup/Time_Conversion.java
+++ HackerRank/Algorithms/Warmup/Time_Conversion.java
@@ -1,27 +1,42 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
 
-public class Time_Conversion 
-{
-
-	public static void main(String[] args) 
-	{
-		Scanner s=new Scanner(System.in);
-		String b=s.nextLine();
-        String ar[]=b.split(":");//07,05,45PM    ===  07,05,45AM
-        String ar2[]=(ar[2].contains("AM"))?(ar[2].split("AM")):(ar[2].split("PM"));//45
-        int hour=Integer.parseInt(ar[0]);
-        if(ar[2].contains("AM"))
+    public static void operation()
         {
-        	ar[0]=(hour==12)?"00":ar[0];
-        		
-        	System.out.println(ar[0]+":"+ar[1]+":"+ar2[0]);
+        String newhour=null,result=null;
+        Scanner in = new Scanner(System.in);
+        String time = in.nextLine();
+        int timelength=time.length();
+        String split[]=time.split(":");
+        int hour=Integer.parseInt(split[0]);
+        int minutes=Integer.parseInt(split[1]);
+        int seconds=Integer.parseInt(split[2].substring(0,split[2].length()-2));
+            if((hour<1 || hour>12)|| (minutes<0 || minutes>59) || (seconds<0 || seconds>59) )
+                        return;
+            if(time.contains("AM") && hour!=12)
+                result=time.substring(0,timelength-2);
+            else 
+            { 
+            if(time.contains("PM"))
+                {
+                if(hour==12)
+                    newhour=String.valueOf(hour);   
+                else
+                    newhour=String.valueOf(hour+12);
+                }
+            else if(hour==12)
+                newhour="00";
+            result=newhour+time.substring(2,timelength-2);
+            }
+        System.out.println(result);    
         }
-        else if(ar[2].contains("PM"))
-        {
-        	ar[0]=((hour!=12)?(((hour+12)<=23)?String.valueOf(hour+12):"00"):"12");
-        	System.out.println(ar[0]+":"+ar[1]+":"+ar2[0]);
-        }
-	}
-
+    public static void main(String[] args)
+    {
+        operation();        
+    }
 }
