b3a81a481164f33e31a03a66d498de3d21dbd305
VijayIndia <vxm47990@ucmo.edu>
2016-12-01 23:56:01 -0500

    Update Simple_Array_Sum.java

--- HackerRank/Algorithms/Warmup/Simple_Array_Sum.java
+++ HackerRank/Algorithms/Warmup/Simple_Array_Sum.java
@@ -1,29 +1,29 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.*;
 class Main
     {
-     Main()
+     public static void operation()
         {
          int sum=0;
          try
              {
-        BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
-             int N=Integer.parseInt(r.readLine());
-             String arr[]=r.readLine().split(" ");
-             for(int i=0;i<N;i++)
-             {
-                 sum+=Integer.parseInt(arr[i]);
-             }
+                BufferedReader r=new BufferedReader(new InputStreamReader(System.in));
+                int N=Integer.parseInt(r.readLine());
+                String arr[]=r.readLine().split(" ");            
+                for(int i=0;i<N;i++)
+                    {
+                        sum+=Integer.parseInt(arr[i]);
+                    }
              System.out.println(sum);
-         }
-         catch(Exception e)
+            }
+         catch(IOException e)
              {
-             
-              }
+             System.out.println(e);
+             }
         }
     public static void main(String args[])
         {
-        Main m=new Main();
-        
+         operation();        
         }
     }
