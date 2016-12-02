a2b73687ce97886429cd977b67b926bc8ecc3be9
VijayIndia <vxm47990@ucmo.edu>
2016-12-02 08:15:09 -0500

    Update Angry_Professor.java

--- HackerRank/Algorithms/Implementation/Angry_Professor.java
+++ HackerRank/Algorithms/Implementation/Angry_Professor.java
@@ -1,41 +1,35 @@
-import java.util.Scanner;
+import java.util.*;
 
-
-public class Angry_Professor 
+public class AngryProfessor 
 {
-	public static void main(String[] args) 
-	{
-		int N=0;//Total Number of Students
-		int K=0;//Expected Number of Students
-	    int T=0;//Number of Testcase
-		String First_Line;//Contains Total Number of Students and Expected Number of Students
-		String Second_Line;//Contains Arrival Time of Each Student
-		Scanner s=new Scanner(System.in);
-		T=Integer.parseInt(s.nextLine());
-		while(T>=1 && T<=10 )
-		{
-			int Present_count=0;//Number of Students Actually present
-			First_Line=s.nextLine();
-			String First_Line_arr[]=First_Line.split(" ");
-			N=Integer.parseInt(First_Line_arr[0]);
-			K=Integer.parseInt(First_Line_arr[1]);
-			if((N>=1 && N<=1000)&&(K>=1 && K<=N))
-			{
-				Second_Line=s.nextLine();
-				String arrival_time[]=Second_Line.split(" ");
-				for(int i=0;i<N;i++)
-				{
-					int x=Integer.parseInt(arrival_time[i]);
-					if(x>=-100 && x<=100)
-						Present_count+=(x<=0)?1:0;
-				}
-				System.out.println((Present_count>=K)?"NO":"YES");
-					
-			}
-			
-		T--;
-		}
-		s.close();
-	}
-
+    public static void input()
+        {
+        Scanner s=new Scanner(System.in);
+        int studentpresent=0,n=0,k=0,i=0;
+        int testcase=Integer.parseInt(s.nextLine());
+        while(testcase>0)
+          {
+        String n_k[]=s.nextLine().split(" ");
+        String input[]=s.nextLine().split(" ");
+        n=Integer.parseInt(n_k[0]);
+        k=Integer.parseInt(n_k[1]);
+        studentpresent=0;
+        for(i=0;i<n;i++)
+            {
+                if((Integer.parseInt(input[i]))<=0)
+                    studentpresent++;
+            }
+        if(studentpresent>=k)
+             System.out.println("NO");
+        else             
+            System.out.println("YES");       
+        testcase--;  
+          }
+        }
+    
+    
+    public static void main(String[] args) 
+    {        
+        input();
+    }
 }
