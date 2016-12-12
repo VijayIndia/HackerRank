9ed904a30a34e132d37d78e0d854dad2d8f478be
Vijay India <vxm47990@ucmo.edu>
2016-12-12 14:03:29 -0500

    Update Two_Strings.java

--- HackerRank/Algorithms/Strings/Two_Strings.java
+++ HackerRank/Algorithms/Strings/Two_Strings.java
@@ -1,63 +1,49 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 
-public class Two_Strings 
+public class Solution 
 {
 
-	public void sort(String a,String b)
-	{
-		int i=0,flag=0,j=0;
-		char arr1[]=a.toCharArray();
-		char arr2[]=b.toCharArray();
-		Arrays.sort(arr1);
-		Arrays.sort(arr2);
-		String arr1_string=new String(arr1);
-		String arr2_string=new String(arr2);
-		int length=((arr1_string.length()<arr2_string.length())?arr1_string.length():arr2_string.length());
-		out:
-		while(i<length)
-		{
-			while(j<length && (arr1_string.charAt(i)>=arr2_string.charAt(j)) )
-			{					
-				if(arr1_string.charAt(i)==arr2_string.charAt(j))
-				{
-					flag=1;
-					System.out.println("YES");
-					break out;
-				}
-				j++;
-			}
-			i++;			
-		}
-		if(flag==0)
-		{
-			System.out.println("NO");
-		}
-	}
-	public static void main(String[] args) 
-	{
-		Two_Strings ts=new Two_Strings();
-		Scanner s=new Scanner(System.in);
-		try
-		{
-		int T=Integer.parseInt(s.nextLine());
-	     while(T>=1 && T<=10)
-	     {
-	    	 String str1=s.nextLine();
-	    	 String str2=s.nextLine();
-	    	 if(str1.length() >=1 &&str2.length()>=1 &&  str1.length()<=100000 && str2.length()<=100000)
-	    	 ts.sort(str1,str2);
-	    	 
-	     }
-		}
-		catch(Exception e)
-		{
-			
-		}
-		finally
-		{
-			s.close();
-		}
-	}
-
+    public boolean operation(String one,String two)
+        {
+        int twolength=two.length();
+        int onelength=one.length();
+        char ch[]=one.toCharArray();
+        Set<Character> onearr=new HashSet<Character>();
+        for(int i=0;i<onelength;i++)
+        		onearr.add(ch[i]);
+        for(int j=0;j<twolength;j++)
+               if(onearr.contains(two.charAt(j)))
+                  return true;
+          return false;
+        }
+    public void input()
+       {
+        String a="",b="";
+        boolean result=false;
+        Scanner s=new Scanner(System.in);
+        int n=Integer.parseInt(s.nextLine());
+        while(n>0)
+          {
+            a="";b="";
+            result=false;
+        a=s.nextLine();
+        b=s.nextLine();
+        int alength=a.length(),blength=b.length();
+        if(alength>=blength)
+           result=operation(a,b);   
+        else
+           result=operation(b,a);
+        if(result)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+        n--;
+        }
+       }
+    public static void main(String[] args) 
+    {
+    	Solution obj=new Solution();      
+        obj.input();
+        
+    }
 }
