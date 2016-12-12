e443d2b6a63faba5cb6a0277bb09aa968d2bdd42
Vijay India <vxm47990@ucmo.edu>
2016-12-12 11:08:27 -0500

    Update Solve_me_First.java

--- HackerRank/Algorithms/Warmup/Solve_me_First.java
+++ HackerRank/Algorithms/Warmup/Solve_me_First.java
@@ -6,27 +6,18 @@ public class Main
         int sum=f+s;
         return sum;
     }
-     public static void main(String []args)
-     {
-         int first_input=0,second_input=0;
+	public void input()
+	{	
+		 int first_input=0,second_input=0,result=0;
          Scanner scan=new Scanner(System.in);
-         try
-             {            
-            first_input=scan.nextInt();
-            second_input=scan.nextInt();
-             }
-         catch(Exception e)
-             {
-             
-             }
-         finally
-             {           
-             scan.close();
-             }
+				first_input=scan.nextInt();
+				second_input=scan.nextInt();
+    			result=solveMeFirst(first_input,second_input);
+                System.out.println(result);
+	}
+     public static void main(String []args)
+     {         
             Main obj=new Main();
-            int result=obj.solveMeFirst(first_input,second_input);
-            System.out.println(result);
-       
-         
+			obj.input();       
      }
 }
