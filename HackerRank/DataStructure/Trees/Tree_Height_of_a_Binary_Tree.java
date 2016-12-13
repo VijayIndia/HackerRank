6fb7332b0c524a5d4963a003beea8e84d4b0d12a
Vijay India <vxm47990@ucmo.edu>
2016-12-13 13:55:49 -0500

    Update Tree_Height_of_a_Binary_Tree.java

--- HackerRank/DataStructure/Trees/Tree_Height_of_a_Binary_Tree.java
+++ HackerRank/DataStructure/Trees/Tree_Height_of_a_Binary_Tree.java
@@ -7,17 +7,14 @@
    */
    int height(Node root)
     {
-       int height_left=0,height_right=0;
-       if(root.left!=null)
-            height_left=1+height(root.left);
-       if(root.right!=null)
-            height_right=1+height(root.right); 
-       else if(root.left==null && root.right==null)
-           return 0;
-       if(height_left>=height_right)
-            return height_left;
-       else if(height_left<height_right)
-           return height_right;
-       else
-           return 0;
+       if(root!=null)
+           {
+                if(root.left!=null && root.right!=null)
+                   return (Math.max(height(root.left),height(root.right))+1);
+                else if(root.right!=null)
+                    return height(root.right)+1;
+                else if(root.left!=null)
+                    return height(root.left)+1;
+            }
+       return 0;
     }
