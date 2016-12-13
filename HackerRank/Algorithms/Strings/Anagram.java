f8ac6b840be9099f7668fbc8feff2717079bf9b4
Vijay India <vxm47990@ucmo.edu>
2016-12-12 23:23:24 -0500

    Update Anagram.java

--- HackerRank/Algorithms/Strings/Anagram.java
+++ HackerRank/Algorithms/Strings/Anagram.java
@@ -1,104 +1,66 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-public class Anagram 
+public class Solution 
 {
-   public void comparison(String First,String Last)
-   {
-	   int i=0,count=0,j=0;
-	   while(i<First.length() &&j<First.length() )
-	   { 
-		   int l=First.charAt(i);
-		   int m=Last.charAt(j);
-				if(l<m && m>=97 && m<=122)
-				{
-					count++;
-					i++;
-				}				
-				else if(l>m && l>=97 && l<=122)
-				{
-					j++;					
-				}
-				else if(l==m)
-				{
-					i++;
-					j++;
-				}
-						  
-		   
-	   }
-	   System.out.println(count+((j-i)>0?(j-i):0));
-   }
-   public String reverse(String Last)
-   {
-	  int Last_length=Last.length();
-	  int Str_length=Last.length()-1;
-	  int i=0;
-	 char rev_string[]=new char[Last_length];
-	  while(i<Last_length)
-	  {
-		  rev_string[i]=Last.charAt(Str_length);
-		  i++;		  
-		  Str_length--;
-	  }
-	  String Last_str= new String(rev_string);
-	  //System.out.println("Last_Str="+Last_str);
-	   return Last_str;
-   }
-   public String sort(String Last)
-   {
-	  char sort_string[]=Last.toCharArray();
-	  Arrays.sort(sort_string);
-	 String s=new String(sort_string);
-	 return s;
-   }
-
-
-	public static void main(String[] args)
-	{
-		Anagram L=new Anagram();
-		Scanner s=new Scanner(System.in);
-		try
-		{
-		int T=Integer.parseInt(s.nextLine());
-		while(T>0)
-		{
-		String str=s.nextLine();
-		int str_length=str.length();
-		String First=null,Last=null;
-			if(str_length>=1 && str_length<=10000)
-				{
-					if(str_length%2==0)
-						{
-							First=str.substring(0,(str_length)/2);
-							Last=str.substring((str_length)/2,str.length());
-							First=L.sort(First);
-							Last=L.sort(Last);
-			
-							if(!(First.equals(Last)))
-								{
-								L.comparison(First,Last);	
-								}
-							else
-								System.out.println("0");
-						}
-					else 
-						System.out.println("-1");
-			   }
-			
-		}
-		
-		T--;
-		s.close();
-		}
-		catch(Exception e)
-		{
-			
-		}
-		finally
-		{
-			s.close();
-		}
-	}
-
+    public void input()
+        {
+        Scanner s=new Scanner(System.in);
+        int n=Integer.parseInt(s.nextLine());
+        String str="";
+        int strlength=0,halflength=0,result=0;
+        if(n>100 || n<1 )
+            return;
+          while(n>0)
+             {
+              str="";
+              strlength=0;
+              halflength=0;
+              result=0;
+                str=s.nextLine();
+                strlength=str.length();
+              if(strlength<1 || strlength>10000)
+                    return;
+                if(strlength%2!=0)
+                     result=-1;
+              else
+                {
+                halflength=strlength/2;
+                String str1=str.substring(0,halflength);
+                String str2=str.substring(halflength,strlength);
+                result=operation(str1,str2);
+                }
+                System.out.println(result);
+                n--;
+             }
+        }
+    public int operation(String one,String two)
+        {
+            int onelength=one.length(),twolength=two.length();
+            int count=0,index=0;
+            char chone[]=one.toCharArray();
+            char chtwo[]=two.toCharArray();
+            Arrays.sort(chone);
+            Arrays.sort(chtwo);
+            List<Character> obj=new ArrayList<Character>();
+            for(int i=0;i<onelength;i++)
+                   obj.add(chone[i]);            
+            for(int j=0;j<twolength;j++)
+                {
+                if(obj.contains(chtwo[j]))
+                    {
+                       index=obj.indexOf(chtwo[j]);
+                       obj.remove(index);
+                    }  
+                }
+            return obj.size();
+        }
+    public static void main(String[] args) 
+    {
+        Solution obj=new Solution();
+        obj.input();
+    }
 }
