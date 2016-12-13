a3f6aef8474a0d24a245a5f2c685234df9db2fc5
Vijay India <vxm47990@ucmo.edu>
2016-12-13 16:24:45 -0500

    Update Intro_Tutorial_Challenges.java

--- HackerRank/Algorithms/Sort/Intro_Tutorial_Challenges.java
+++ HackerRank/Algorithms/Sort/Intro_Tutorial_Challenges.java
@@ -2,34 +2,38 @@ import java.util.Scanner;
 public class Intro_Tutorial_Challenges 
 {
 
+	public void input()
+	{
+	Scanner s=new Scanner(System.in);
+    	int no_to_find=s.nextInt();
+     	int array_size=s.nextInt();
+     	Integer arr[]=new Integer[array_size];
+     	in:
+     		if(no_to_find>=1 && no_to_find<=1000)
+     			{
+    	 			for(int i=0;i<array_size;i++)
+    	 				{
+    		 				arr[i]=s.nextInt();
+    		 				if(!(arr[i]>=-10000 && arr[i]<=10000))
+    			 				break in;
+    	 				}
+    	 			 int j=0;
+     	 out:
+    	 				while(j<arr.length)
+    	 					{
+    	 						if(arr[j]==no_to_find)
+    		 						{
+    		 							System.out.println(j);
+    		 							break out;
+    		 						}
+    	 						j++;
+    	 					}
+			}
+	}
 	
 	public static void main(String[] args) 
 	{
-     Scanner s=new Scanner(System.in);
-     int no_to_find=s.nextInt();
-     int array_size=s.nextInt();
-     Integer arr[]=new Integer[array_size];
-     in:
-     if(no_to_find>=1 && no_to_find<=1000)
-     {
-    	 for(int i=0;i<array_size;i++)
-    	 	{
-    		 arr[i]=s.nextInt();
-    		 if(!(arr[i]>=-10000 && arr[i]<=10000))
-    			 break in;
-    	 	}
-    	 int j=0;
-     out:
-    	 while(j<arr.length)
-    	 {
-    	 if(arr[j]==no_to_find)
-    		 {
-    		 System.out.println(j);
-    		 break out;
-    		 }
-    	 j++;
-    	 }
-	}
-
-  }
+     	Intro_Tutorial_Challenges obj=new Intro_Tutorial_Challenges();
+	obj.input();
+  	}
 }
