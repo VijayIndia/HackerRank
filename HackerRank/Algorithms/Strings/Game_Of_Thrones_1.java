959ef0af0980df031d34afd351601e9163110792
Vijay India <vxm47990@ucmo.edu>
2016-12-13 00:55:03 -0500

    Update Game_Of_Thrones_1.java

--- HackerRank/Algorithms/Strings/Game_Of_Thrones_1.java
+++ HackerRank/Algorithms/Strings/Game_Of_Thrones_1.java
@@ -1,69 +1,33 @@
 import java.util.*;
-public class Game_Of_Thrones_1 
+
+public class Solution 
 {
-	public void comparison(String str)
-	{
-		int i=0,twice=0,once=0,flag=0;
-		out:
-		while(i<str.length()-1)
-		{
-		if(str.charAt(i)==str.charAt(i+1))
-			{
-		//	System.out.println("("+str.charAt(i)+","+str.charAt(i+1)+")");
-			i=i+2;
-			twice++;
-			//System.out.println("i="+i);
-			}
-		else if(str.charAt(i)!=str.charAt(i+1))
-			{
-			once++;
-			i++;
-			/*System.out.println("In second block,i="+i);
-				if(count==2)
-					{
-					flag=1;
-					System.out.println("NO");
-					break out;
-					}*/
-			}
-		else 
-			i++;
-		}
-		//System.out.println("Its out of the loop");
-		if(str.length()/2==twice || ((str.length()/2)-1)==twice)
-		{
-			if(str.length()%2==0 && twice==str.length()/2)
-				System.out.println("YES");
-			else if(str.length()%2==1 && (twice==str.length()/2 || once==1))
-				System.out.println("YES");
-			else if(once>=1)
-				System.out.println("NO");
-        
-		}
-            else
-                System.out.println("NO");
-	}
-	 public String sort(String Last)
-	   {
-		  char sort_string[]=Last.toCharArray();
-		  Arrays.sort(sort_string);
-		 String s=new String(sort_string);
-		 return s;
-	   }
-
-	public static void main(String[] args) 
-	{
-      Scanner s=new Scanner(System.in);     
-    	  String str=s.nextLine();
-          Game_Of_Thrones_1 g=new Game_Of_Thrones_1();
-
-    	  if(str.length()<=100000 && str.length()>=1)
-    	  {
-    		 str=g.sort(str);
-    		// System.out.println("sorted="+str);
-    		  g.comparison(str);
-    	  }
-    	  s.close();
-	}
-
+    public boolean input()
+        {
+            Scanner S=new Scanner(System.in);
+            ArrayList<Character> m=new ArrayList<Character>();
+            String str=S.nextLine();
+            int strlength=str.length();
+            char ch[]=str.toCharArray();
+        for(int i=0;i<strlength;i++)
+            {
+                if(m.contains(ch[i]))
+                    m.remove(m.indexOf(ch[i]));
+                else
+                    m.add(ch[i]);
+            }
+            if(m.size()<=1)
+                return true;
+           return false;             
+        }
+    public static void main(String[] args) 
+    {
+        boolean result=false;
+        Solution obj=new Solution();
+        result=obj.input();
+        if(result)
+           System.out.println("YES");
+        else
+           System.out.println("NO");
+    }
 }
