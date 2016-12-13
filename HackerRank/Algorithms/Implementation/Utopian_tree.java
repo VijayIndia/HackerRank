f9c4f8051f68fa959e0c2caf7050897c4aec2e2e
Vijay India <vxm47990@ucmo.edu>
2016-12-13 10:43:27 -0500

    Update Utopian_tree.java

--- HackerRank/Algorithms/Implementation/Utopian_tree.java
+++ HackerRank/Algorithms/Implementation/Utopian_tree.java
@@ -1,37 +1,38 @@
-import java.util.Scanner;
-public class Tree_Sapling
-{
-	
-	public void Tree_Sapling(int x,int n,int limit)
-	{
-		//System.out.println("x="+x+"n="+n);
-		if(limit>0 && n%2!=0 && n<=limit)
-		{			
-			Tree_Sapling(x*2,++n,limit);
-		}
-		else if(limit>0 && n%2==0 && n<=limit)
-		{
-			Tree_Sapling(x+1,++n,limit);
-		}
-		else if(n>limit)
-		{
-			System.out.println(x);
-		}
-	}
-	
-	
-	public static void main(String[] args) 
-	{
-		Scanner s=new Scanner(System.in);
-	    Tree_Sapling t=new Tree_Sapling();
-		int N=s.nextInt();
-	   while(N>=1 && N<=10)
-	   {
-		   int input=s.nextInt();
-		 t.Tree_Sapling(1,1,(0<=input && input<=60)?input:-1);
-	    N--;
-	   }
-	   s.close();
-	}
+import java.util.*;
 
+public class Solution 
+{
+ public static void input()
+    {
+    int result=0,testcase=0;
+        Scanner in = new Scanner(System.in);
+        testcase = in.nextInt();
+    while(testcase>0)
+       {
+        result=operation(1,in.nextInt());
+        System.out.println(result);
+        testcase--;
+        }
+    }
+    public static int operation(int height,int cycle)
+       {  
+         if(cycle>0)
+           {
+              height*=2;
+              cycle--;
+
+            }
+           if(cycle>0)
+            {
+            height++;
+            height=operation(height,--cycle);
+            }
+         
+   
+        return height;
+       }
+    public static void main(String[] args) 
+    { 
+        input();
+    }
 }
