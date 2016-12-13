ac72713c5ad815ffe8ba6947b1d296ec81776cbe
Vijay India <vxm47990@ucmo.edu>
2016-12-13 13:59:28 -0500

    Update Binary_Search_Tree_Lowest_Common_Ancestor.java

--- HackerRank/DataStructure/Trees/Binary_Search_Tree_Lowest_Common_Ancestor.java
+++ HackerRank/DataStructure/Trees/Binary_Search_Tree_Lowest_Common_Ancestor.java
@@ -9,25 +9,19 @@
     */
 static Node lca(Node root,int v1,int v2)
     {
-    Node temp=root;
-    if(root.left!=null && root.left.right!=null && root.left.left!=null && (root.left.data==v1 || root.left.data==v2) && ( (root.left.right.data==v1 || root.left.right.data==v2)||(root.left.left.data==v1 || root.left.left.data==v2)))
-        return root;
-    else if(v1<root.data && v2<root.data)
-        {
-        if(root.left!=null)
-            temp=lca(root.left,v1,v2);
-        }
-    else if((root.left!=null && root.right!=null) && (v1==root.left.data && v2==root.right.data) ||(v1==root.right.data && v2==root.left.data))
-        {       
-            return root;
-        }    
-    else if(v1>root.data || v2>root.data)
-        {
-        return root;
-        }
+    Node temp=null;
+      if(root.data>v1 && root.data >v2 )//case 1
+          {
+          temp=lca(root.left,v1,v2);
+          }
+    else if(root.data<v1 && root.data <v2) //case 2
+       {
+          temp=lca(root.right,v1,v2);
+       }
+    else if(root.data>=v1 && root.data<=v2 || root.data<=v1 && root.data>=v2)//case 3
+       {
+       return root; 
+       }
     return temp;
+       
     }
-
-
-
-
