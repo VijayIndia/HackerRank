67ad23e0555d8bf020ec4e1907d4d0bc37475623
Vijay India <vxm47990@ucmo.edu>
2016-12-12 22:04:49 -0500

    Update Alternating_Characters.java

--- HackerRank/Algorithms/Strings/Alternating_Characters.java
+++ HackerRank/Algorithms/Strings/Alternating_Characters.java
@@ -1,48 +1,35 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-
-public class Alternating_Characters 
+public class Solution 
 {
-	static int flag=3;
-	static int count=0;
-	
-	public void str(char ch)
-	{
-	 if(ch=='A' && (flag==1 || flag==3))
-	 {
-		 flag=0;
-	 }
-	 else if(ch=='B' && (flag==0 || flag==3))
-	 {
-		 flag=1;
-	 }
-	 else if((ch=='A' && (flag==0)) || (ch=='B' && flag==1) )
-	 {
-		 count++;
-	 }
-		
-		
-	}
-	
-	public static void main(String[] args) 
-	{
-	Alternating_Characters ac=new Alternating_Characters();
-    Scanner s=new Scanner(System.in);
-    int T=Integer.parseInt(s.nextLine());
-    while(T>0)
+    public void input()
+        {
+        Scanner s=new Scanner(System.in);
+        int n=Integer.parseInt(s.nextLine());
+        while(n>0)
+          {
+        String str=s.nextLine();
+        int strlength=str.length(),count=0;
+        char ch[]=str.toCharArray();
+        Stack<Character> st=new Stack<Character>();
+        for(int i=0;i<strlength;i++)
+            {
+                if(st.isEmpty() || st.peek()!=ch[i])
+                    st.push(ch[i]);
+            else
+                count++;
+            }
+            System.out.println(count);
+            n--;
+          }
+        }
+    public static void main(String[] args) 
     {
-    String x=s.nextLine();
-    int x_length=x.length()-1;
-    	while((x_length)>=0)
-    		{
-    			ac.str(x.charAt(x_length));
-    			x_length--;
-    		}
-        System.out.println(count);
-        flag=3;
-        count=0;
-    	T--;
+        Solution s=new Solution();
+        s.input();
     }
-	}
-
 }
