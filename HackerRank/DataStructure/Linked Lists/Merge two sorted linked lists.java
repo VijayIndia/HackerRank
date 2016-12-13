271f10f7b257ce07b9a54034680fcd5fd59de322
Vijay India <vxm47990@ucmo.edu>
2016-12-13 14:19:58 -0500

    Update Merge two sorted linked lists.java

--- HackerRank/DataStructure/Linked Lists/Merge two sorted linked lists.java
+++ HackerRank/DataStructure/Linked Lists/Merge two sorted linked lists.java
@@ -9,41 +9,19 @@
 */
     
 Node MergeLists(Node headA, Node headB) 
-{                                     
-    Node tempA=headA,tempB=headB,prev=null,tempB_temp=null,tempA_temp=null;
-    int flag=0;
-    out:
-    while(tempA!=null && tempB!=null)
-        {     
-          tempB_temp=tempB.next;
-         while(tempA!=null && tempA.data<tempB.data && flag==0)
-            {
-            prev=tempA;
-             if(tempA.next!=null)
-                   tempA=tempA.next;
-             else
-                 flag=1;
-            }
-       if(tempA.data>=tempB.data)
-            {
-                tempA_temp=tempA;
-                tempB.next=tempA;
-           if(prev!=null)     
-                prev.next=tempB;           
-                tempA=tempB;  
-               if(tempA_temp==headA)
-                 headA=tempA;
-            } 
-        else
+    {
+        if(headA==null && headB==null)
+            return null;
+        if(headA==null)
+                return headB;
+        else if(headB==null)
+                return headA;            
+        if(headA.data<=headB.data)
+            headA.next=MergeLists(headA.next,headB);
+        if(headB.data<headA.data)
            {
-            tempA.next=tempB;
-            break out;
-            }
-              tempB=tempB_temp;
-       
-        }
-    if(tempA==null)
-        return headB;
-     
-    return headA;
-}
+               headB.next=MergeLists(headA,headB.next);
+               headA=headB;
+           }
+         return headA;
+    }
